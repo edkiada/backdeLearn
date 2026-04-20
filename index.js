db748537ef69a9d2c61c1fd5231dbd0cf9296e07
edkiada <yyh901277111@gmail.com>
2026-04-21 00:15:46 +0800

    t

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ let persons = [
     "number": "39-23-6423122"
   }
 ]
-app.use(express.static('dist'))
+//app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
