565b9bf545a432e0355d162e99ee1b0f7e2942a0
edkiada <yyh901277111@gmail.com>
2026-04-21 11:37:12 +0800

    mongo

--- index.js
+++ index.js
@@ -25,6 +25,24 @@ let persons = [
     "number": "39-23-6423122"
   }
 ]
+
+/*-----------------------*/
+const mongoose = require('mongoose')
+
+// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
+const password = process.argv[2]
+const url = `mongodb+srv://fullstack:${password}@cluster0.a5qfl.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+/*-----------------------*/
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
@@ -76,6 +94,12 @@ app.get('/ping', (req, res) => {
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
+})
+
+app.get('/api/notes', (req, res) => {
+  Note.find({}).then(notes => {
+    res.json(notes)
+  })
 })
 
 app.get('/api/persons/:id', (req, res) => {
